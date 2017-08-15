89a41fc5dd615b81758054b914287634e236fe0e
usama ahmed <uusa35@gmail.com>
2017-08-15 13:41:11 +0300

    adjust final issues

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>7orof.com</title>
+        <title>{{ env('APP_NAME') }}</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -41,7 +41,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">{{ trans('general.under_construction') }}</div>
+                <div class="title">Under Construction !!</div>
             </div>
         </div>
     </body>
