c79c58fffaad2c9d985921f2670d0aba6673c47c
usama ahmed <uusa35@gmail.com>
2018-01-18 19:14:29 +0300

    done with register and editing user

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::group(['middleware' => ['auth', 'ActiveUser']], function () {
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('approve', 'HomeController@toggleApprove')->name('quotation.approve');
     Route::resource('quotation', 'QuotationController', ['except' => 'destroy']);
+    Route::get('send', 'QuotationController@send')->name('quotation.send');
     Route::group(['middleware' => 'AdminAccessOnly'], function () {
         Route::resource('quotation', 'QuotationController', ['only' => 'destroy']);
-        Route::get('send', 'QuotationController@send')->name('quotation.send');
         Route::get('accept', 'QuotationController@accept')->name('toggle.accept');
         Route::get('approve', 'HomeController@toggleApprove')->name('toggle.approve');
         Route::get('admin', 'HomeController@toggleAdmin')->name('toggle.admin');
